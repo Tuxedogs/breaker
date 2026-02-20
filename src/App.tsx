3095fcd7c6d3fefcee4e936210c0ccb1fc726405
Tux <cattux29@gmail.com>
2026-02-19 19:05:50 -0600

    hero behavior sessionstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import AppShell from "./components/AppShell";
+import DocsShell from "./components/DocsShell";
 import DoctrineFirstFramework from "./pages/DoctrineFirstFramework";
 import HeroOpening from "./pages/HeroOpening";
 import WipPage from "./pages/WipPage";
+
+const ENTRY_STORAGE_KEY = "ares:entered-framework";
+
+function EntryGate() {
+  const hasEntered = sessionStorage.getItem(ENTRY_STORAGE_KEY) === "true";
+  if (hasEntered) {
+    return <Navigate to="/framework" replace />;
+  }
+
+  return <HeroOpening />;
+}
 
 export default function App() {
   return (
     <Routes>
       <Route element={<AppShell />}>
-        <Route index element={<HeroOpening />} />
+        <Route index element={<EntryGate />} />
         <Route path="framework" element={<DoctrineFirstFramework />} />
-        <Route path="ships/:shipId" element={<WipPage section="Ships" />} />
-        <Route path="systems/:systemId" element={<WipPage section="Systems" />} />
+        <Route path="docs/*" element={<DocsShell />} />
+
+        <Route path="ships/perseus" element={<WipPage section="Ships" title="Perseus" />} />
+        <Route path="ships/polaris" element={<WipPage section="Ships" title="Polaris" />} />
+        <Route path="ships/idris" element={<WipPage section="Ships" title="Idris" />} />
+
+        <Route path="systems/sub-targeting" element={<WipPage section="Systems" title="Sub Targeting" />} />
+        <Route path="systems/turret-keybinds" element={<WipPage section="Systems" title="Turret Keybinds" />} />
+        <Route
+          path="systems/turret-keybinds/additional"
+          element={<WipPage section="Systems" title="Additional Settings & Binds" />}
+        />
+        <Route path="systems/gunnery-with-luna" element={<WipPage section="Systems" title="Gunnery with Luna" />} />
+        <Route path="systems/communications" element={<WipPage section="Systems" title="Communications" />} />
+        <Route
+          path="systems/additional-resources"
+          element={<WipPage section="Systems" title="Additional Resources" />}
+        />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
