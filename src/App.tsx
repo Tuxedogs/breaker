bc9dcaacbfaff7b7157295e3ec46aee93af80537
Tux <cattux29@gmail.com>
2026-02-22 01:47:35 -0600

    gunnery with luna page and transition cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import DoctrineLanding from "./pages/DoctrineLanding";
 import HeroOpening from "./pages/HeroOpening";
 import PerseusPage from "./pages/ships/PerseusPage";
 import AdditionalSettingsPage from "./pages/systems/AdditionalSettingsPage";
+import GunneryWithLunaPage from "./pages/systems/GunneryWithLunaPage";
 import SubTargetingPage from "./pages/systems/SubTargetingPage";
 import TurretKeybindsPage from "./pages/systems/TurretKeybindsPage";
 import WipPage from "./pages/WipPage";
@@ -39,7 +40,7 @@ export default function App() {
           path="systems/turret-keybinds/additional"
           element={<Navigate to="/systems/additional-settings-binds" replace />}
         />
-        <Route path="systems/gunnery-with-luna" element={<WipPage section="Systems" title="Gunnery with Luna" />} />
+        <Route path="systems/gunnery-with-luna" element={<GunneryWithLunaPage />} />
         <Route path="systems/communications" element={<WipPage section="Systems" title="Communications" />} />
         <Route
           path="systems/additional-resources"
