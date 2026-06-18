b49443229a635c20653802e39bc4bbd36c9117e1
Tux <cattux29@gmail.com>
2026-06-18 05:01:51 -0500

    Consolidate mission API function for Vercel deploy

--- api/missions/family/[familyKey]/variants.ts
+++
@@ -1,7 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
-
-import { runMissionsApiHandler } from "../../../../server/routes/missionsApi.js";
-
-export default async function handler(request: IncomingMessage, response: ServerResponse) {
-  await runMissionsApiHandler(request, response);
-}
